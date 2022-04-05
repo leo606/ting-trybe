904fbe71f23e1bae196fcac7cb32066a9fcbf5e3
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-04-05 10:50:56 -0300

    req 02 - file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+TXT_EXTENSION = 'txt'
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_extension = path_file.split('.')[-1].lower()
+    if file_extension != TXT_EXTENSION:
+        return print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file) as file:
+            lines = file.read()
+            return lines.split('\n')
+    except Exception:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
