4bce1c40db850869cda4474e5e05dc5e7d309df2
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-04-05 12:09:53 -0300

    req 03 - file process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_content = txt_importer(path_file)
+
+    if instance.is_file_in_queue(path_file):
+        print('file already in queue', file=sys.stderr)
+    else:
+        file_processed = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file_content),
+            "linhas_do_arquivo": file_content
+        }
+        instance.enqueue(file_processed)
+        print(file_processed)
 
 
 def remove(instance):
