65928663c7736fd8db089df01297c6c3f862c56c
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-04-05 15:06:49 -0300

    req 07 - word search - search by word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,20 +3,23 @@ import re
 # from ting_file_management.queue import Queue
 
 
-def word_occurrences(word, lines):
+def word_occurrences(word, lines, return_content):
     matched = []
     reg_pattern = re.compile(word, re.IGNORECASE)
 
     for index, line in enumerate(lines):
         if re.search(reg_pattern, line):
-            matched.append({'linha': index + 1})
+            if return_content:
+                matched.append({'linha': index + 1, 'conteudo': line})
+            else:
+                matched.append({'linha': index + 1})
     return matched
 
 
 def exists_word(word, instance):
     exists_word_report = []
     for file in instance:
-        ocurrences = word_occurrences(word, file['linhas_do_arquivo'])
+        ocurrences = word_occurrences(word, file['linhas_do_arquivo'], False)
         if len(ocurrences):
             exists_word_report.append({
                 "palavra": word,
@@ -27,4 +30,13 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_word_report = []
+    for file in instance:
+        ocurrences = word_occurrences(word, file['linhas_do_arquivo'], True)
+        if len(ocurrences):
+            search_word_report.append({
+                "palavra": word,
+                "arquivo": file['nome_do_arquivo'],
+                "ocorrencias": ocurrences,
+            })
+    return search_word_report
